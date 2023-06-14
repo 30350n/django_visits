8957b667ee4e0a2c793671552500b00422e75746
Bobbe <maxschlecht@web.de>
2023-06-14 02:55:21 +0200

    Add days arg to last_visits templatetag

--- templatetags/last_visits.py
+++ templatetags/last_visits.py
@@ -6,7 +6,7 @@ from django import template
 register = template.Library()
 
 @register.simple_tag
-def last_visits(n=128):
-    date_threshold = timezone.now() - timedelta(days=1)
+def last_visits(n=128, days=7):
+    date_threshold = timezone.now() - timedelta(days=days)
     visits = Visit.objects.filter(is_crawler=False, timestamp__gt=date_threshold)
     return sorted(visits, reverse=True)[:n]
